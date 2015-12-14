6fa91010e86524c9eb7abe9c522ee3bf8b4dc05b
helgahilm13 <helga13@ru.is>
2015-12-14 12:44:04 +0000

    working on the edit code for computers

--- vika3/ui/editcomputer.cpp
+++ vika3/ui/editcomputer.cpp
@@ -1,14 +1,40 @@
 #include "editcomputer.h"
 #include "ui_editcomputer.h"
+#include "QMessageBox"
+
 
 editComputer::editComputer(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::editComputer)
 {
     ui->setupUi(this);
+
 }
 
 editComputer::~editComputer()
 {
     delete ui;
+}
+
+void editComputer::on_pushButton_editcomputer_clicked()
+{
+    QString name, wasItBuilt, buildYear, type, description;
+    name = ui->label_name->text();
+    wasItBuilt = ui->label_buildYear->text();
+    buildYear = ui->label_buildYear->text();
+    type = ui->label_type->text();
+    description = ui->label_description->text();
+
+    QSqlQuery query;
+    query.prepare("update Computers set computer_name='"+name+"', build_year='"+buildYear+"', computer_type='"+type+"', was_built= '"+wasItBuilt+"', description= '"+description+"'");
+
+    if(query.exec())
+    {
+        QMessageBox::critical(this,tr("Edit"),tr("Update"));
+    }
+    else
+    {
+       QMessageBox::critical(this,tr("error::"),query.lastError().text());
+    }
+
 }
